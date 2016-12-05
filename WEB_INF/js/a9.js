90bf97492c43edbd618ab5f07f79f19ad36b5ed6
movery <mike.overy13@gmail.com>
2016-12-05 15:01:14 -0500

    Initial Scrabble Update

---
+++ WEB_INF/js/a9.js
@@ -0,0 +1,170 @@
+var scrabbleTiles = [
+    {"letter" : "A", "value": 1,  "max" : 9,  "current" : 9  },
+    {"letter" : "B", "value": 3,  "max" : 2,  "current" : 2  },
+    {"letter" : "C", "value": 3,  "max" : 2,  "current" : 2  },
+    {"letter" : "D", "value": 2,  "max" : 4,  "current" : 4  },
+    {"letter" : "E", "value": 1,  "max" : 12, "current" : 12 },
+    {"letter" : "F", "value": 4,  "max" : 2,  "current" : 2  },
+    {"letter" : "G", "value": 2,  "max" : 3,  "current" : 3  },
+    {"letter" : "H", "value": 4,  "max" : 2,  "current" : 2  },
+    {"letter" : "I", "value": 1,  "max" : 9,  "current" : 9  },
+    {"letter" : "J", "value": 8,  "max" : 1,  "current" : 1  },
+    {"letter" : "K", "value": 5,  "max" : 1,  "current" : 1  },
+    {"letter" : "L", "value": 1,  "max" : 4,  "current" : 4  },
+    {"letter" : "M", "value": 3,  "max" : 2,  "current" : 2  },
+    {"letter" : "N", "value": 1,  "max" : 6,  "current" : 6  },
+    {"letter" : "O", "value": 1,  "max" : 8,  "current" : 8  },
+    {"letter" : "P", "value": 3,  "max" : 2,  "current" : 2  },
+    {"letter" : "Q", "value": 10, "max" : 1,  "current" : 1  },
+    {"letter" : "R", "value": 1,  "max" : 6,  "current" : 6  },
+    {"letter" : "S", "value": 1,  "max" : 4,  "current" : 4  },
+    {"letter" : "T", "value": 1,  "max" : 6,  "current" : 6  },
+    {"letter" : "U", "value": 1,  "max" : 4,  "current" : 4  },
+    {"letter" : "V", "value": 4,  "max" : 2,  "current" : 2  },
+    {"letter" : "W", "value": 4,  "max" : 2,  "current" : 2  },
+    {"letter" : "X", "value": 18, "max" : 1,  "current" : 1  },
+    {"letter" : "Y", "value": 4,  "max" : 2,  "current" : 2  },
+    {"letter" : "Z", "value": 10, "max" : 1,  "current" : 1  },
+    {"letter" : "_", "value": 0,  "max" : 2,  "current" : 2  },
+
+];
+
+var deckOfTiles = []
+function initializeDeckOfTiles() {
+    /* Push all tiles into an array to "Draw" from */
+    for (var i = 0; i < scrabbleTiles.length; ++i) {
+	for (var j = 0; j < scrabbleTiles[i].max; ++j) {
+	    var tile =  {"letter"   : scrabbleTiles[i].letter, 
+			 "value"    : scrabbleTiles[i].value,
+			 "modifier" : "",
+			 "id"       : scrabbleTiles[i].letter + j};
+			
+	    deckOfTiles.push(tile);
+	}
+    }
+
+    /* Shuffle an array: https://www.frankmitchell.org/2015/01/fisher-yates/ */
+    for (var i = deckOfTiles.length - 1; i > 0; --i) {
+	var j = Math.floor(Math.random() * (i + 1));
+	var temp = deckOfTiles[i];
+	deckOfTiles[i] = deckOfTiles[j];
+	deckOfTiles[j] = temp;
+    }
+}
+
+var handOfTiles = []
+function drawTiles() {
+    for (var i = handOfTiles.length; i < 7; ++i) {
+	var tile = deckOfTiles.pop();
+
+	var img = "<img class='tile' id='" + tile.id + "' src='WEB_INF/img/Scrabble/Scrabble_Tile_" + tile.letter + ".jpg'></img>";
+
+	$("#scrabbleRack").append(img);
+	$("#" + tile.id).draggable({
+	    revert: "invalid",
+	});
+	
+	handOfTiles.push(tile);
+    }
+}
+
+var tilesInPlay = []
+function initializeDropLocations() {
+    $("td").droppable({
+	accept: ".tile",
+
+	/* Referenced: http://stackoverflow.com/a/6003729 */
+	drop: function(ev, ui) {
+            $(ui.draggable).detach().css({top: 1,left: 3}).appendTo(this);
+
+	    /* Check if tile comes from hand. If so, adjust storage devices */
+	    for(var i = 0; i < handOfTiles.length; ++i) {
+		if (handOfTiles[i].id == ui.draggable.attr("id")) {
+		    tilesInPlay.push(handOfTiles[i]);
+		    handOfTiles.splice(i, 1);
+		}
+	    }
+
+	    /* Adjust the modifier for any and all tiles */
+	    for(var i = 0; i < tilesInPlay.length; ++i) {
+		if (tilesInPlay[i].id == ui.draggable.attr("id")) {
+		    tilesInPlay[i].modifier = $(this).attr("class");
+		}
+	    }
+		
+	    adjustScore();
+	}
+    });
+
+    $("#scrabbleRack").droppable({
+	accept: ".tile",
+
+	drop: function(ev, ui) {
+	    /* Check if tile comes in play. If so, adjust storage devices */
+	    for(var i = 0; i < tilesInPlay.length; ++i) {
+		if (tilesInPlay[i].id == ui.draggable.attr("id")) {
+		    handOfTiles.push(tilesInPlay[i]);
+		    tilesInPlay.splice(i, 1);
+		}
+
+		adjustScore();
+	    }
+	}
+    });
+				
+}
+
+var totalScore = 0;
+function adjustScore() {
+    var score = 0;
+    var doubleWordCount = 0;
+    var tripleWordCount = 0;
+
+    for(var i = 0; i < tilesInPlay.length; ++i) {
+	switch(tilesInPlay[i].modifier) {
+	case "doubleLetter ui-droppable":
+	    score += tilesInPlay[i].value * 2;
+	    break;
+	case "tripleLetter ui-droppable":
+	    score += tilesInPlay[i].value * 3;
+	    break;
+	case "doubleWord ui-droppable":
+	    score += tilesInPlay[i].value;
+	    doubleWordCount += 1;	    
+	    break;
+	case "tripleWord ui-droppable":
+	    score += tilesInPlay[i].value;
+	    tripleWordCount += 1;
+	    break;
+	default:
+	    score += tilesInPlay[i].value;
+	    break;
+	}
+    }
+
+    for (var i = 0; i < doubleWordCount; ++i)
+	score *= 2;
+
+    for (var i = 0; i < tripleWordCount; ++i)
+	score *= 3;
+    
+    return score;
+}
+
+function submitTiles() {
+    totalScore = adjustScore();
+
+    for (var i = 0; i < tilesInPlay.length; ++i) {
+	$("#" + tilesInPlay[i].id).draggable("destroy");
+	console.log("wew");
+    }
+    tilesInPlay = []
+
+    drawTiles();
+}
+
+$(document).ready(function() {
+    initializeDeckOfTiles();
+    initializeDropLocations();
+    drawTiles();
+});
